75078dde9929fd22263b2d6d93f00347931980ad
Acorneld <acorneld@gmail.com>
2022-02-26 10:11:08 -0700

    Skeleton with getters and setters

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -2,4 +2,50 @@ package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
 
+	private static int numericId = 1;
+	private String foodTruckName;
+	private String foodType;
+	private double foodTruckRating;
+	
+	public FoodTruck() {
+}
+	public FoodTruck(int numericId, String foodTruckName, String foodType, double foodTruckRating) {
+		FoodTruck.numericId = numericId;
+		this.foodTruckName = foodTruckName;
+		this.foodType = foodType;
+		this.foodTruckRating = foodTruckRating;
+}
+
+	public int getNumericId() {
+		return numericId;
+	}
+	public void setNumericId(int numericId) {
+		FoodTruck.numericId = numericId;
+	}
+	
+	public String getFoodTruckName() {
+		return foodTruckName;
+	}
+	public void setFoodTruckName(String foodTruckName) {
+		this.foodTruckName = foodTruckName;
+	}
+	
+	public String getFoodType() {
+		return foodType;
+	}
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	
+	public double getFoodTruckRating() {
+		return foodTruckRating;
+	}
+	public void setFoodTruckingRating(double foodTruckRating) {
+		this.foodTruckRating = foodTruckRating;
+	}
+
+
+
+
+
 }
