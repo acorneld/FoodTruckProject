2b3462c3790690f44acf9310206baa3684c41e15
Acorneld <acorneld@gmail.com>
2022-02-26 12:39:33 -0700

    Working array and first Truck

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,52 +1,66 @@
 package com.skilldistillery.foodtruck;
-import java.util.Scanner;
+
+
 public class FoodTruck {
 
-	private int numericId = 1;
+	private static int numericId = 1;
 	private String foodTruckName;
 	private String foodType;
 	private double foodTruckRating;
-	
+
 	public FoodTruck() {
-}
-	
+	}
+
 	public FoodTruck(int numericId, String foodTruckName, String foodType, double foodTruckRating) {
-		this.numericId = numericId;
+		FoodTruck.numericId = numericId;
 		this.foodTruckName = foodTruckName;
 		this.foodType = foodType;
 		this.foodTruckRating = foodTruckRating;
-}
+	}
 
 	public int getNumericId() {
 		return numericId;
 	}
+
 	public void setNumericId(int numericId) {
-		this.numericId = numericId;
+		FoodTruck.numericId = numericId;
 	}
-	
+
 	public String getFoodTruckName() {
-		return foodTruckName;
-	}
+		
+			return foodTruckName;
+		}
+	
+
 	public void setFoodTruckName(String foodTruckName) {
 		this.foodTruckName = foodTruckName;
 	}
-	
+
 	public String getFoodType() {
 		return foodType;
 	}
+
 	public void setFoodType(String foodType) {
 		this.foodType = foodType;
 	}
-	
+
 	public double getFoodTruckRating() {
 		return foodTruckRating;
 	}
+
 	public void setFoodTruckRating(double foodTruckRating) {
 		this.foodTruckRating = foodTruckRating;
 	}
 
-
-
-
-
+	public String toString() {
+		String output = "Numeric ID:" + numericId + " Food Truck Name:" + foodTruckName + " Food type:" + foodType
+				+ " Rating:" + foodTruckRating;
+		return output;
+	}
+	public void displayFoodTruck(String output) {
+		String truckData = toString();
+		System.out.println(truckData);
+	}
+	
+	
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -1,22 +1,38 @@
 package com.skilldistillery.foodtruck;
+
 import java.util.Scanner;
+
 public class FoodTruckApp {
-	
-	
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		
-		FoodTruck ft1 = new FoodTruck();
-		ft1.setNumericId(1);
-		System.out.println("Please enter the name of the Foodtruck: ");
-		ft1.setFoodTruckName(kb.nextLine());
-		System.out.println("Please enter the type of food served: ");
-		ft1.setFoodType(kb.nextLine());
-		System.out.println("Please enter a rating of 1-10 ");
-		ft1.setFoodTruckRating(kb.nextInt());
-	
-		
+
+		FoodTruck[] truckArray = new FoodTruck[5];
+
+		for (int i = 0; i < truckArray.length; i++) {
+
+			FoodTruck ft1 = new FoodTruck();
+			System.out.println("Please enter the name of the Foodtruck: ");
+			String truckName = kb.nextLine();
+			if (truckName.equals("quit")) {
+				break;
+
+			} else {
+				ft1.setFoodTruckName(truckName);
+			}
+			System.out.println("Please enter the type of food served: ");
+			String foodTypeName = kb.nextLine();
+			ft1.setFoodType(foodTypeName);
+			System.out.println("Please enter a rating of 1-10 ");
+			int foodTruckRatingName = kb.nextInt();
+			System.out.println("");
+			ft1.setFoodTruckRating(foodTruckRatingName);
+
+			String ft1Data = ft1.toString();
+			ft1.displayFoodTruck(ft1Data);
+		}
+
+		kb.close();
 	}
 
 }
